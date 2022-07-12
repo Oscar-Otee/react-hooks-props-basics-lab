692056be2a77d403cf8818fa694ca40a295e1f64
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-07-13 01:01:57 +0300

    commit tree 48fcb7929d9014913ca5587b5c6423742dc74cfd

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,11 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color= {user.color} name={user.name} city={user.city} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
 
 export default App;
+
